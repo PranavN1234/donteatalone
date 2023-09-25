ab2f92a4fc9e3ccd84d47ec87ffa0e05da5c7a13
PranavN1234 <pranavnarayan79@gmail.com>
2023-09-25 03:08:16 -0400

    First proj

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController: ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Appuser>>> GetUsers(){
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Appuser>> GetUser(int id){
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+}
---
+++ API/data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Appuser> Users { get; set; }
+}
