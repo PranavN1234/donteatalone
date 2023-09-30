ad47fccb6c789e8dc13bd28a6d6f6c4ad9064ede
PranavN1234 <pranavnarayan79@gmail.com>
2023-09-30 05:53:32 -0400

    Frontend changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = "Don't eat alone";
   users: any;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService) {
 
   }
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('http://localhost:5001/api/users').subscribe({
-      next: response=> this.users = response,
-      error: ()=>{console.log("error")},
-      complete:()=>{console.log("Request has been completed")}
+      next: response => this.users = response,
+      error: () => { console.log("error") },
+      complete: () => { console.log("Request has been completed") }
     })
+  }
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user')!)
+    if(!user){
+      return;
+    }
+
+    this.accountService.setCurrentUser(user);
   }
 
 }
